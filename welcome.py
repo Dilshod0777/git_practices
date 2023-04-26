2c2bdd53ea9193b934de03fcea5de272c2942f8a
Dilshod0777 <dilshod0777@users.noreply.github.com>
2023-04-26 14:36:28 -0500

    dilshodB changes

--- welcome.py
+++ welcome.py
@@ -1,14 +1,8 @@
-
-
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1>WELCOME TO TURAN CYBER HUB</h1>"
-
-
-
-
     app.run(host="0.0.0.0", port=80)
 
    
@@ -17,3 +11,6 @@ def hello():
    new ones adde
 
    line added to dilshod barnh
+
+   new lines going to be added
+   
