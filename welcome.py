aa86bab7ba2a142c84e766e31a8078cb6709cbaf
Dilshod0777 <dilshod0777@users.noreply.github.com>
2023-04-26 13:27:34 -0500

    latest change

--- welcome.py
+++ welcome.py
@@ -8,8 +8,6 @@ def hello():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
 
-   cxsgcsgkcxfkjgscfxkjgscj
-   gxcsmgxcmsgcxmgscxmgsCVX,
-   XGSCSGXC,SJVX,sSC,avs,jhcv
-   cjcv,hjavHJCV,JHCV,Dhc,jdv
+   
+   all new changes
 
